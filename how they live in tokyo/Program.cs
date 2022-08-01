806d2aeddc0f9b226ce7461edb862f03f19eabe8
DG64 <DG64@DESKTOP-325D3AA>
2022-08-01 10:00:31 +0700

    Add project files.

---
+++ how they live in tokyo/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace how_they_live_in_tokyo
+{
+    public static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            using (var game = new Game1())
+                game.Run();
+        }
+    }
+}
